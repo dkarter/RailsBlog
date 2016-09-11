5ceb9514c67c5789dda9974b5940b025e48ad903
Dorian Karter <dkarter@gmail.com>
2016-09-11 16:19:43 -0500

    Set login path as root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
   resources :sessions, only: [:new, :create]
+
   get '/login', as: 'login', to: 'sessions#new'
+
+  root to: 'sessions#new'
 end
